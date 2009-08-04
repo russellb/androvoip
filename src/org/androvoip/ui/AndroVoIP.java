493e27ed1c5b7e0eb72cc35a60cac90590ddb0ad
Russell Bryant <russell@russellbryant.net>
2009-08-04 00:16:46 -0500

    Trivial change to test something with push ...

--- src/org/androvoip/ui/AndroVoIP.java
+++ src/org/androvoip/ui/AndroVoIP.java
@@ -31,7 +31,6 @@ import android.widget.TabHost;
 import android.os.Bundle;
 
 public class AndroVoIP extends TabActivity {	
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
