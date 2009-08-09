0abd51b6db422817e3220d86f599887ceab312ce
Russell Bryant <russell@russellbryant.net>
2009-08-09 14:05:48 -0500

    Run "clean up", mostly adding this qualifiers to member variable usage.

--- src/org/androvoip/iax2/IAX2Service.java
+++ src/org/androvoip/iax2/IAX2Service.java
@@ -26,12 +26,12 @@ import java.net.SocketException;
 import org.androvoip.audio.AndroidAudioInterface;
 import org.androvoip.ui.Settings;
 
-import com.mexuar.corraleta.protocol.netse.BinderSE;
-
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
+
+import com.mexuar.corraleta.protocol.netse.BinderSE;
 
 public class IAX2Service extends Service {
 	private String last_host;
@@ -49,83 +49,83 @@ public class IAX2Service extends Service {
 	@Override
 	public void onCreate() {
 		Log.d("IAX2Service", "onCreate(), initializing vars.");
-		last_host = "";
-		last_username = "";
-		last_password = "";
-		binder = null;
-		registered = false;
-		audio_interface = new AndroidAudioInterface();
+		this.last_host = "";
+		this.last_username = "";
+		this.last_password = "";
+		this.binder = null;
+		this.registered = false;
+		this.audio_interface = new AndroidAudioInterface();
 	}
-	
+
 	/**
 	 * Called due to a call to startService().
 	 * <p>
-	 * This function is a bit hackish right now, but it's a start.
-	 * Essentially, every time the AndroVoIP Activities decide that the
-	 * IAX2 service should refresh itself and check to see if it needs to
-	 * change registration parameters, it calls startService().  Note that
-	 * this will get called on every startService() call, but onCreate() is
-	 * what only gets called once when a call to startService() results in
-	 * the service starting up.
+	 * This function is a bit hackish right now, but it's a start. Essentially,
+	 * every time the AndroVoIP Activities decide that the IAX2 service should
+	 * refresh itself and check to see if it needs to change registration
+	 * parameters, it calls startService(). Note that this will get called on
+	 * every startService() call, but onCreate() is what only gets called once
+	 * when a call to startService() results in the service starting up.
 	 * <p>
-	 * Eventually, we should move to creating an API for interacting with
-	 * this service.  Instead of startService(), Activities will bind to the
-	 * service using bindService().  With this approach, a service can provide
-	 * an API for the activity to interact with it after binding.
+	 * Eventually, we should move to creating an API for interacting with this
+	 * service. Instead of startService(), Activities will bind to the service
+	 * using bindService(). With this approach, a service can provide an API for
+	 * the activity to interact with it after binding.
 	 */
 	@Override
 	public void onStart(Intent arg0, int start_id) {
 		final String host = get_config_param("host");
 		final String username = get_config_param("username");
 		final String password = get_config_param("password");
-		
+
 		Log.d("IAX2Service", "onStart: host - " + host);
-		Log.d("IAX2Service", "onStart: last_host - " + last_host);
+		Log.d("IAX2Service", "onStart: last_host - " + this.last_host);
 		Log.d("IAX2Service", "onStart: username - " + username);
-		Log.d("IAX2Service", "onStart: last_username - " + last_username);
+		Log.d("IAX2Service", "onStart: last_username - " + this.last_username);
 		Log.d("IAX2Service", "onStart: password - " + password);
-		Log.d("IAX2Service", "onStart: last_password - " + last_password);
-		
+		Log.d("IAX2Service", "onStart: last_password - " + this.last_password);
+
 		/* If there is no host set, or the host changed, kill off the binder. */
-		if (host.equals("") || !host.equals(last_host)) {
-			if (binder != null) {
-				binder.stop();
-				binder = null;
+		if (host.equals("") || !host.equals(this.last_host)) {
+			if (this.binder != null) {
+				this.binder.stop();
+				this.binder = null;
 			}
 			if (host.equals("")) {
 				return;
 			}
 		}
-		
+
 		/* Start the binder if we have not done so already. */
 		try {
-			if (binder == null) {
-				binder = new BinderSE(host, audio_interface);
-				last_host = host;
-				last_username = "";
-				last_password = "";
+			if (this.binder == null) {
+				this.binder = new BinderSE(host, this.audio_interface);
+				this.last_host = host;
+				this.last_username = "";
+				this.last_password = "";
 			}
-		} catch (SocketException e) {
+		} catch (final SocketException e) {
 			e.printStackTrace();
 		}
-		
-		if (username.equals(last_username) && password.equals(last_password) && registered) {
+
+		if (username.equals(this.last_username)
+				&& password.equals(this.last_password) && this.registered) {
 			return;
 		}
-		
+
 		try {
-			if (registered) {
-				binder.unregister(null);		
+			if (this.registered) {
+				this.binder.unregister(null);
 			}
-			binder.register(username, password, null, true);
-			last_username = username;
-			last_password = password;
-			registered = true;
-		} catch (Exception e) {
+			this.binder.register(username, password, null, true);
+			this.last_username = username;
+			this.last_password = password;
+			this.registered = true;
+		} catch (final Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private String get_config_param(String arg) {
 		return getSharedPreferences(Settings.PREFS_FILE, MODE_PRIVATE)
 				.getString(arg, "");
