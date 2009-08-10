60721bea21ddcae20a843b4701e992e7f171c27b
Russell Bryant <russell@russellbryant.net>
2009-08-09 19:35:08 -0500

    Add comments.

--- src/org/androvoip/audio/AndroidAudioInterface.java
+++ src/org/androvoip/audio/AndroidAudioInterface.java
@@ -30,98 +30,158 @@ import com.mexuar.corraleta.protocol.AudioSender;
 import com.mexuar.corraleta.protocol.VoiceFrame;
 
 public class AndroidAudioInterface implements AudioInterface {
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#changedProps()
+	 */
 	public void changedProps() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#cleanUp()
+	 */
 	public void cleanUp() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#codecPrefString()
+	 */
 	public String codecPrefString() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#getByFormat(java.lang.Integer)
+	 */
 	public AudioInterface getByFormat(Integer format) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#getFormatBit()
+	 */
 	public int getFormatBit() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#getSampSz()
+	 */
 	public int getSampSz() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#playAudioStream(java.io.InputStream)
+	 */
 	public void playAudioStream(InputStream in) throws IOException {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#readDirect(byte[])
+	 */
 	public long readDirect(byte[] buff) throws IOException {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#readWithTime(byte[])
+	 */
 	public long readWithTime(byte[] buff) throws IOException {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#sampleRecord(com.mexuar.corraleta.audio.SampleListener)
+	 */
 	public void sampleRecord(SampleListener list) throws IOException {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#setAudioSender(com.mexuar.corraleta.protocol.AudioSender)
+	 */
 	public void setAudioSender(AudioSender as) {
 		// TODO Auto-generated method stub
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#startPlay()
+	 */
 	public void startPlay() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#startRec()
+	 */
 	public long startRec() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#startRinging()
+	 */
 	public void startRinging() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#stopPlay()
+	 */
 	public void stopPlay() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#stopRec()
+	 */
 	public void stopRec() {
 		// TODO Auto-generated method stub
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#stopRinging()
+	 */
 	public void stopRinging() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#supportedCodecs()
+	 */
 	public Integer supportedCodecs() {
 		return new Integer(VoiceFrame.LIN16_BIT);
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#write(byte[], long)
+	 */
 	public void write(byte[] buff, long timestamp) throws IOException {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * @see com.mexuar.corraleta.audio.AudioInterface#writeDirect(byte[])
+	 */
 	public void writeDirect(byte[] buff) throws IOException {
 		// TODO Auto-generated method stub
 		
