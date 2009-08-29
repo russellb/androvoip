0b022fd8d0c0adae7848ca3b0a7837e5a2e516b0
Russell Bryant <russell@russellbryant.net>
2009-08-29 11:22:47 -0500

    Commit progress on audio playback.  It plays jittery static very well.

--- src/org/androvoip/iax2/AndroidAudioInterface.java
+++ src/org/androvoip/iax2/AndroidAudioInterface.java
@@ -2,22 +2,22 @@
  * AndroVoIP -- VoIP for Android.
  *
  * Copyright (C), 2006, Mexuar Technologies Ltd.
- *   -- Adapted from Audio8k.java
- *   
+ *   -- Partially adapted from Audio8k.java
+ *
  * Copyright (C), 2009, Russell Bryant
- * 
+ *
  * Russell Bryant <russell@russellbryant.net>
- * 
+ *
  * AndroVoIP is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * AndroVoIP is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with AndroVoIP.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -26,7 +26,12 @@ package org.androvoip.iax2;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.Queue;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.util.Log;
 
 import com.mexuar.corraleta.audio.AudioInterface;
@@ -35,6 +40,12 @@ import com.mexuar.corraleta.protocol.AudioSender;
 import com.mexuar.corraleta.protocol.VoiceFrame;
 
 public class AndroidAudioInterface implements AudioInterface {
+	/* Data for the receive path */
+	private AudioTrack track = null;
+	private Thread play_thread = null;
+	private Queue<byte[]> playQ = new LinkedList<byte[]>();
+
+	/* Data for the transmit path */
 	private AudioSender as = null;
 	private Thread rec_thread = null;
 
@@ -42,7 +53,7 @@ public class AndroidAudioInterface implements AudioInterface {
 	 * Audio properties have changed.
 	 * <p>
 	 * No active code uses this.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#changedProps()
 	 */
 	public void changedProps() {
@@ -53,7 +64,7 @@ public class AndroidAudioInterface implements AudioInterface {
 	 * Clean up after the parent BinderSE is stopped.
 	 * <p>
 	 * Called by com.mexuar.corraleta.protocol.netse.BinderSE.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#cleanUp()
 	 */
 	public void cleanUp() {
@@ -64,7 +75,7 @@ public class AndroidAudioInterface implements AudioInterface {
 	 * Generate codec preferences string.
 	 * <p>
 	 * Called by com.mexuar.corraleta.protocol.ProtocolControlFrame.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#codecPrefString()
 	 * @see com.mexuar.corraleta.audio.javasound.Audio8k#codecPrefString()
 	 */
@@ -84,7 +95,7 @@ public class AndroidAudioInterface implements AudioInterface {
 	 * built in to this class.
 	 * <p>
 	 * Called by com.mexuar.corraleta.protocol.Call.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#getByFormat(java.lang.Integer)
 	 */
 	public AudioInterface getByFormat(Integer format) {
@@ -100,7 +111,7 @@ public class AndroidAudioInterface implements AudioInterface {
 	 * Return the bit for the format of this AudioInterface.
 	 * <p>
 	 * Called by com.mexuar.corraleta.protocol.Call.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#getFormatBit()
 	 */
 	public int getFormatBit() {
@@ -109,7 +120,7 @@ public class AndroidAudioInterface implements AudioInterface {
 
 	/**
 	 * Called by com.mexuar.corraleta.protocol.Call.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#getSampSz()
 	 */
 	public int getSampSz() {
@@ -118,7 +129,7 @@ public class AndroidAudioInterface implements AudioInterface {
 
 	/**
 	 * No active code uses this.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#playAudioStream(java.io.InputStream)
 	 */
 	public void playAudioStream(InputStream in) throws IOException {
@@ -127,7 +138,7 @@ public class AndroidAudioInterface implements AudioInterface {
 
 	/**
 	 * No active code uses this.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#readDirect(byte[])
 	 */
 	public long readDirect(byte[] buff) throws IOException {
@@ -164,9 +175,58 @@ public class AndroidAudioInterface implements AudioInterface {
 		}
 	}
 
+	private void writeBuff(byte[] buf) {
+		if (this.track == null) {
+			Log.w("IAX2Audio", "write() without an AudioTrack");
+			return;
+		}
+
+		// Log.d("IAX2Audio", "write() buf of len " + Integer.toString(buf.length));
+
+		int written = 0;
+		while (written < buf.length) {
+			int res;
+
+			res = this.track.write(buf, written, buf.length - written);
+			switch (res) {
+			case AudioTrack.ERROR_INVALID_OPERATION:
+				Log.e("IAX2Audio", "Invalid write()");
+				return;
+			case AudioTrack.ERROR_BAD_VALUE:
+				Log.e("IAX2Audio", "Bad arguments to write()");
+				return;
+			}
+
+			written += res;
+		}
+	}
+
+	private void playbackTime() {
+		byte[] buf;
+
+		while ((buf = this.playQ.poll()) != null) {
+			writeBuff(buf);
+		}
+	}
+
+	private void playTick() {
+		final long delta = 20;
+
+        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO);
+		
+		while (this.play_thread != null) {
+			playbackTime();
+			try {
+				Thread.sleep(delta);
+			} catch (final InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	/**
 	 * Called by com.mexuar.corraleta.protocol.AudioSender
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#readWithTime(byte[])
 	 */
 	public long readWithTime(byte[] buff) throws IOException {
@@ -175,7 +235,7 @@ public class AndroidAudioInterface implements AudioInterface {
 
 	/**
 	 * No active code uses this.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#sampleRecord(com.mexuar.corraleta.audio.SampleListener)
 	 */
 	public void sampleRecord(SampleListener list) throws IOException {
@@ -184,11 +244,13 @@ public class AndroidAudioInterface implements AudioInterface {
 
 	/**
 	 * Used by com.mexuar.corraleta.protocol.Call.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#setAudioSender(com.mexuar.corraleta.protocol.AudioSender)
 	 */
 	public void setAudioSender(AudioSender as) {
 		this.as = as;
+
+		/* Set up audio recording (transmit) */
 
 		final Runnable trec = new Runnable() {
 			public void run() {
@@ -197,13 +259,46 @@ public class AndroidAudioInterface implements AudioInterface {
 		};
 
 		this.rec_thread = new Thread(trec, "rec_thread");
-		this.rec_thread.setPriority(Thread.MAX_PRIORITY - 1);
 		this.rec_thread.start();
+
+		/* Set up audio playback (receive) */
+
+		if (track != null) {
+			Log.w("IAX2Audio", "track not null in setAudioSender()");
+		}
+
+		final int minBuffer = AudioTrack.getMinBufferSize(8000,
+								AudioFormat.CHANNEL_CONFIGURATION_MONO,
+								AudioFormat.ENCODING_PCM_16BIT);
+
+		try {
+			this.track = new AudioTrack(AudioManager.STREAM_MUSIC,
+								8000, /* 8 kHz sample rate */
+								AudioFormat.CHANNEL_CONFIGURATION_MONO,
+								AudioFormat.ENCODING_PCM_16BIT,
+								(640 > minBuffer) ? 640 : minBuffer,
+								AudioTrack.MODE_STREAM);
+		} catch (final IllegalArgumentException e) {
+			Log.e("IAX2Audio", "Failed to create AudioTrack");
+			e.printStackTrace();
+			return;
+		}
+
+		final Runnable tplay = new Runnable() {
+			public void run() {
+				playTick();
+			}
+		};
+
+		this.play_thread = new Thread(tplay, "play_thread");
+		this.play_thread.start();
+
+		track.play();
 	}
 
 	/**
 	 * No active code uses this.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#startPlay()
 	 */
 	public void startPlay() {
@@ -212,7 +307,7 @@ public class AndroidAudioInterface implements AudioInterface {
 
 	/**
 	 * Used by com.mexuar.corraleta.protocol.Call.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#startRec()
 	 */
 	public long startRec() {
@@ -222,7 +317,7 @@ public class AndroidAudioInterface implements AudioInterface {
 
 	/**
 	 * Used by com.mexuar.corraleta.protocol.Call.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#startRinging()
 	 */
 	public void startRinging() {
@@ -231,24 +326,42 @@ public class AndroidAudioInterface implements AudioInterface {
 
 	/**
 	 * Used by com.mexuar.corraleta.protocol.Call.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#stopPlay()
 	 */
 	public void stopPlay() {
 		Log.d("IAX2Audio", "stopPlay()");
+
+		if (track != null) {
+			track.stop();
+		}
+
+		try {
+			if (this.play_thread != null) {
+				final Thread t = this.play_thread;
+				/* Setting this to null is the signal to the thread to exit. */
+				this.play_thread = null;
+				t.join();
+			}
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		track = null;
 	}
 
 	/**
 	 * Stop sending our audio.
 	 * <p>
 	 * Used by com.mexuar.corraleta.protocol.Call.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#stopRec()
 	 */
 	public void stopRec() {
 		try {
 			if (this.rec_thread != null) {
 				final Thread t = this.rec_thread;
+				/* Setting this to null is the signal to the thread to exit. */
 				this.rec_thread = null;
 				t.join();
 			}
@@ -261,7 +374,7 @@ public class AndroidAudioInterface implements AudioInterface {
 
 	/**
 	 * Used by com.mexuar.corraleta.protocol.Call.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#stopRinging()
 	 */
 	public void stopRinging() {
@@ -272,7 +385,7 @@ public class AndroidAudioInterface implements AudioInterface {
 	 * Return an Integer bit mask of the codecs we support.
 	 * <p>
 	 * Used by ProtocolControlFrameNew.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#supportedCodecs()
 	 */
 	public Integer supportedCodecs() {
@@ -283,18 +396,18 @@ public class AndroidAudioInterface implements AudioInterface {
 	 * Handle received audio.
 	 * <p>
 	 * Used by com.mexuar.corraleta.protocol.Call.
-	 * 
-	 * TODO Do something useful with the received audio.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#write(byte[], long)
 	 */
-	public void write(byte[] buff, long timestamp) throws IOException {
-		/* Drop it on the floor! */
+	public void write(byte[] buf, long timestamp) throws IOException {
+		byte[] qBuf = new byte[buf.length];
+		System.arraycopy(buf, 0, qBuf, 0, buf.length);
+		this.playQ.offer(qBuf);
 	}
 
 	/**
 	 * No active code uses this right now.
-	 * 
+	 *
 	 * @see com.mexuar.corraleta.audio.AudioInterface#writeDirect(byte[])
 	 */
 	public void writeDirect(byte[] buff) throws IOException {
